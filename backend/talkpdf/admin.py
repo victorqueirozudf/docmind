8d2789c45fd62e478be4b818c7c4493ca4e3b04f
victor.queiroz <victor.queiroz@cs.udf.edu.br>
2024-09-25 16:46:08 -0300

    Alteração de arquivos

--- backend/talkpdf/admin.py
+++ backend/talkpdf/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from .models import UserPdf
+from .models import MessageTalkPdf
 
 # Register your models here.
-admin.site.register(UserPdf)
+admin.site.register(MessageTalkPdf)
